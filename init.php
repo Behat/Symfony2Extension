8a074e85a9655f722b7b9f7d604bbbe907083270
everzet <ever.zet@gmail.com>
2012-05-18 14:53:04 +0200

    fixed init

--- init.php
+++ init.php
@@ -9,14 +9,14 @@
  * with this source code in the file LICENSE.
  */
 
-require __DIR__.'/src/Behat/Symfony2Extension/Compiler/KernelLoaderPass.php';
+require __DIR__.'/src/Behat/Symfony2Extension/Compiler/KernelInitializationPass.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Console/Processor/InitProcessor.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Console/Processor/LocatorProcessor.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Context/ClassGuesser/BundleContextClassGuesser.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Context/Initializer/KernelAwareInitializer.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Context/KernelAwareInterface.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Context/KernelDictionary.php';
-require __DIR__.'/src/Behat/Symfony2Extension/Driver/HttpKernelDriver.php';
+require __DIR__.'/src/Behat/Symfony2Extension/Driver/KernelDriver.php';
 require __DIR__.'/src/Behat/Symfony2Extension/Driver/Extension.php';
 
 return new Behat\Symfony2Extension\Extension;
