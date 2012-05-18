fd5a9ba8411e65f8323983a81f675894f2e8ecfc
everzet <ever.zet@gmail.com>
2012-05-18 17:22:11 +0200

    bootstraped test project

---
+++ testapp/app/AppKernel.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__.'/autoload.php';
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
