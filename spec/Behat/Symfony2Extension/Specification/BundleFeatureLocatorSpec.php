e08611b7c515b6e8c96c7ad5403d85f3e3afe0d2
everzet <ever.zet@gmail.com>
2014-03-13 08:23:38 +0000

    Fix broken BundleFeatureLocator spec

--- spec/Behat/Symfony2Extension/Specification/BundleFeatureLocatorSpec.php
+++ spec/Behat/Symfony2Extension/Specification/BundleFeatureLocatorSpec.php
@@ -4,19 +4,21 @@ namespace spec\Behat\Symfony2Extension\Specification;
 
 use Behat\Gherkin\Gherkin;
 use Behat\Symfony2Extension\Suite\SymfonyBundleSuite;
+use Behat\Testwork\Specification\Locator\SpecificationLocator;
+use Behat\Testwork\Specification\SpecificationIterator;
 use Behat\Testwork\Suite\Suite;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 class BundleFeatureLocatorSpec extends ObjectBehavior
 {
-    function let(Gherkin $gherkin, SymfonyBundleSuite $suite, BundleInterface $bundle)
+    function let(SpecificationLocator $baseLocator, SymfonyBundleSuite $suite, BundleInterface $bundle)
     {
-        $this->beConstructedWith($gherkin, __DIR__);
+        $this->beConstructedWith($baseLocator);
 
         $suite->getBundle()->willReturn($bundle);
         $bundle->getName()->willReturn('AcmeDemoBundle');
-        $bundle->getPath()->willReturn(__DIR__ . '/src/Acme/DemoBundle');
+        $bundle->getPath()->willReturn('/src/Acme/DemoBundle');
     }
 
     function it_is_a_specification_locator()
@@ -43,5 +45,12 @@ class BundleFeatureLocatorSpec extends ObjectBehavior
         $iterator = $this->locateSpecifications($suite, '@MyBundle');
         $iterator->shouldBeAnInstanceOf('Behat\Testwork\Specification\NoSpecificationsIterator');
         $iterator->getSuite()->shouldBe($suite);
+    }
+
+    function it_proxies_call_to_base_locator_if_bundle_locator_is_correct(SpecificationLocator $baseLocator, SymfonyBundleSuite $suite, SpecificationIterator $iterator)
+    {
+        $baseLocator->locateSpecifications($suite, '/src/Acme/DemoBundle/Features/my.feature')->willReturn($iterator);
+
+        $this->locateSpecifications($suite, '@AcmeDemoBundle/my.feature')->shouldReturn($iterator);
     }
 }
