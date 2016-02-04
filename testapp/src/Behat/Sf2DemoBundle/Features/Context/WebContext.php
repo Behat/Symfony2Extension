1026330f3c28cb93f96c6a23d313919beb65b29c
Sascha Schimke <sascha@schimke.me>
2016-02-04 22:14:01 +0100

    allow arrays as arguments in ServiceArgumentResolver – example in the sample test application

--- testapp/src/Behat/Sf2DemoBundle/Features/Context/WebContext.php
+++ testapp/src/Behat/Sf2DemoBundle/Features/Context/WebContext.php
@@ -11,7 +11,7 @@ class WebContext extends MinkContext implements KernelAwareContext
 {
     private $kernel;
 
-    public function __construct(Session $session, $simpleParameter, $simpleArg)
+    public function __construct(Session $session, $simpleParameter, $simpleArg, array $services, array $params)
     {
     }
 
